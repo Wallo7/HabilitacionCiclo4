361d71ef44dc05e585006af38267c55b289f03e1
carlos garcia <91922693+Wallo7@users.noreply.github.com>
2021-12-22 19:52:06 -0500

    sibida habilitacion

---
+++ src/main/java/com/example/habilitacion/productrepository/ProductRepository.java
@@ -0,0 +1,44 @@
+package com.example.habilitacion.productrepository;
+
+import com.example.habilitacion.crudRepository.ProductCrudRepository;
+import com.example.habilitacion.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductRepository {
+    @Autowired
+    private ProductCrudRepository productCrudRepository;
+
+    public List<Product> getAll(){
+        return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public Optional<Product> getProduct(String reference){
+        return productCrudRepository.findById(reference);
+    }
+
+    public Product save(Product product){
+        return productCrudRepository.save(product);
+    }
+
+    public void update(Product product){
+        productCrudRepository.save(product);
+    }
+
+    public void delete(Product product){
+        productCrudRepository.delete(product);
+    }
+
+    public List<Product> findByDescriptionContainingIgnoreCase(String description){
+        return productCrudRepository.findByDescriptionContainingIgnoreCase(description);
+    }
+
+    public List<Product> findByPriceLessThanEqual(Double price){
+        return productCrudRepository.findByPriceLessThanEqual(price);
+    }
+
+}
